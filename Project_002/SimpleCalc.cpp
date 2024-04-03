f34dfddd1808abe731dbbb6cdca0bbf31d6651ac
Andhika Soebroto <andhikareyhan@gmail.com>
2024-04-03 12:06:10 +0700

    Completed my second project - simple addition calculator

---
+++ Project_002/SimpleCalc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int x, y;
+int sum;
+
+int main() {
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Enter another number: ";
+    cin >> y;
+    sum = x + y;
+
+    cout << "The total is: " << sum;
+    return 0;
+}
